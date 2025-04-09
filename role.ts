a062f54a60a1c2195241fa70d605b6791139fc1a
Thakur Ad <45841417+thakuad@users.noreply.github.com>
2025-04-09 12:16:09 +1000

    Create role.ts

---
+++ role.ts
@@ -0,0 +1,25 @@
+// user-role.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, firstValueFrom } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class UserRoleService {
+  private rolesSubject = new BehaviorSubject<string[]>([]);
+  public roles$ = this.rolesSubject.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  async loadRoles(): Promise<void> {
+    const roles = await firstValueFrom(this.http.get<string[]>('/api/user/roles'));
+    this.rolesSubject.next(roles);
+  }
+
+  get roles(): string[] {
+    return this.rolesSubject.value;
+  }
+
+  hasRole(role: string): boolean {
+    return this.roles.includes(role);
+  }
+}
